148ab2156a9537575f1f92ed8aeda3849564f131
diegoromanortiz <68930460+diegoromanortiz@users.noreply.github.com>
2021-09-24 02:22:58 -0300

    Update models.py

--- flaskAppBuilder/app/models.py
+++ flaskAppBuilder/app/models.py
@@ -7,6 +7,6 @@ from sqlalchemy.orm import relationship
 You can use the extra Flask-AppBuilder fields and Mixin's
 
 AuditMixin will add automatic timestamp of created and modified by who
-
+vvvvvvvvvv
 
 """
